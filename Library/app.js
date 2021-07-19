ef593e90efc3537fc394be531c3ce1c6be23bb47
atreat738 <atreat738@gmail.com>
2021-07-19 17:28:35 -0500

    Added some validation, styles, and a change read button.

--- Library/app.js
+++ Library/app.js
@@ -5,6 +5,7 @@ const readInput = document.querySelector('#Read');
 const bookList = document.querySelector('#book-list');
 
 const addBookButton = document.querySelector('#formSubmitBtn'); 
+const readButton = document.querySelectorAll('.readButton');
 const deleteBookButton = document.querySelectorAll('.deleteButton');
 
 const theHobbit = new Book('The Hobbit', 'JRR Tolken', 295, 'No');
@@ -14,10 +15,10 @@ let myLibrary = [theHobbit, underTheDome];
 displayBook();
 
 function Book(title, author, pages, read) {
- this.title = title;
- this.author = author;
- this.pages = pages;
- this.read = read;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
 //Create a function that stores input into myLibrary Array
@@ -26,12 +27,12 @@ function addBookToLibrary() {
     let author = authorInput.value;
     let pages = pagesInput.value;
     let read = checkReadStatus(readInput);
-    let newBook = new Book(title, author, pages, read);
-    console.log(newBook);
-    console.log(myLibrary);
-    myLibrary.push(newBook); 
-    displayBook();
-    clearForm();
+    if (validate(title, author, pages) == true) {
+        let newBook = new Book(title, author, pages, read);
+        myLibrary.push(newBook); 
+        displayBook();
+        clearForm();
+    }
 }
 
 function checkReadStatus(readInput) {
@@ -56,51 +57,77 @@ addBookButton.addEventListener('click', addBookToLibrary);
 function displayBook() {
     bookList.innerText = "";
     myLibrary.forEach(function(Book) {
-            //display the book.title 
-            let displayRow = document.createElement('tr');
-            displayRow.textContent = Book.title;
-            bookList.appendChild(displayRow);
-            //display book author
-            let displayAuthor = document.createElement('td');
-            displayAuthor.textContent = Book.author;
-            displayRow.appendChild(displayAuthor);
-            //display pages
-            let displayPages = document.createElement('td');
-            displayPages.textContent = Book.pages;
-            displayRow.appendChild(displayPages);
-            //display read status
-            let displayRead = document.createElement('td');
-            displayRead.textContent = Book.read;
-            displayRow.appendChild(displayRead);
-            //display delete button
-            let deleteButton = document.createElement('button');
-            deleteButton.textContent = 'Delete';
-            deleteButton.classList.add('deleteButton');
-            displayRow.appendChild(deleteButton);
-        
+        //display the book title 
+        let displayRow = document.createElement('tr');
+        displayRow.textContent = Book.title;
+        bookList.appendChild(displayRow);
+        //display book author
+        let displayAuthor = document.createElement('td');
+        displayAuthor.textContent = Book.author;
+        displayRow.appendChild(displayAuthor);
+        //display pages
+        let displayPages = document.createElement('td');
+        displayPages.textContent = Book.pages;
+        displayRow.appendChild(displayPages);
+        //display read status
+        let readCell = document.createElement('td')
+        let displayRead = document.createElement('button');
+        displayRead.textContent = Book.read;
+        if(Book.read == 'Yes') {
+            displayRead.classList.add('readYesButton');
+            displayRead.classList.add('readButton');
+        } else if(Book.read == 'No') {
+            displayRead.classList.add('readNoButton');
+            displayRead.classList.add('readButton');
+        }
+        displayRow.appendChild(readCell);
+        readCell.appendChild(displayRead);
+        //display delete button
+        let deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.classList.add('deleteButton');
+        displayRow.appendChild(deleteButton);        
     });
+}
 
+//Will fix this once I read up on prototypes again
+ Book.prototype.toggleRead = function(currentBook) {
+    if(currentBook.classList.contains('readButton')) {
+            if(Book.read == 'yes') {
+                return Book.read = 'No'; 
+            } else if (Book.read == 'No') {
+                return Book.read = 'Yes';
+        }
+    }
+ }
+
+function deleteBook(currentBook) {
+    if(currentBook.classList.contains('deleteButton')) {
+        myLibrary.splice(myLibrary[currentBook], 1);
+        currentBook.parentElement.remove(); 
+    //console.log(myLibrary);
+    }
+}
+
+bookList.addEventListener('click', (e) => {
+    deleteBook(e.target);
+    Book.prototype.toggleRead(e.target);
+});
+
+//Validation
+function validate(title, author, pages) {
+    if (bookTitleInput.value == "" || authorInput.value == "" || pagesInput.value == "") {
+        alert('Please fill out all of the text fields');
+    //The else If condition below tests if pagesInput is a whole number
+    } else if (pagesInput.value % 1 != 0) {
+        alert('Pages must be a whole number');
+        return false;
+    } else {
+        return true;
+    }
 }
 
 
-// function deleteBook(currentBook) {
-//     currentBook = this.Book;  
-//     console.log(currentBook);
-//     this.title = null;
-//     this.author = null;
-//     this.pages = null;
-//     this.read = null;
-//     this.deleteButton = null;
-// }
-
-// deleteBookButton.addEventListener('click', deleteBook);
-
-// function checkbox() {
-//     let checked = false;
-//     if(document.querySelector('#Read:checked')) {
-//         checked = true;
-//     }
-// }
 
 
 
